703ea5675d3c915e851ee5263f62f4c7da74922d
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:16 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostOptionalConan(ConanFile):
     name = "Boost.Optional"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/optional"
+    url = "https://github.com/bincrafters/conan-boost-optional"
+    source_url = "https://github.com/boostorg/optional"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     requires =  "Boost.Level5Group/1.64.0@bincrafters/testing"
